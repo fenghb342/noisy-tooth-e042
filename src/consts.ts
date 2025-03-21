c893d1a9525a6b4255bfd11b3c6912cd0fc962b3
Mr.FengHongBo <154956318+fenghb342@users.noreply.github.com>
2025-03-22 00:04:34 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "冯大帅的Blog";
+export const SITE_DESCRIPTION = "Hello everyone！ Welcome to my website! This is my first website,hope It's useful!";
